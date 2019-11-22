b773c381655ee22d85e0861dec848f36b13ee1d6
Jim Frohnhofer <jfrohnhofer@kabbage.com>
2019-11-22 14:31:33 -0500

    init

--- stop_dev.sh
+++ stop_dev.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+export POSTGRES_VOLUME=/tmp/fijpg
+export PLAY_LOG_VOLUME=/tmp
+
+export AWS_SMTP_USERNAME=FixMe
+export AWS_SMTP_PASSWORD=FixMe
+
+export SCHED_SERVER_PORT=8073
+export NEWS_SERVER_PORT=8074
+export SCRAPE_SERVER_PORT=8075
+
 docker-compose down
