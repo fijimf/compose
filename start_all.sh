91d67f2a49e67e43a3a929608073ffeeb831b768
Jim Frohnhofer <jfrohnhofer@kabbage.com>
2020-03-08 01:09:15 -0500

    detach

--- start_all.sh
+++ start_all.sh
@@ -3,11 +3,9 @@
 export POSTGRES_VOLUME=/tmp/fijpg2
 export PLAY_APPLICATION_DIR=/tmp/play
 export PLAY_APPLICATION_LOG=/tmp/logs
-env
-export AWS_SMTP_USERNAME
-export AWS_SMTP_PASSWORD
 
 source ./ports.sh
 
+env
 
 /usr/local/bin/docker-compose up --force-recreate --detach
